852cfd88bbd7a2080cc119dbf50986a7c6fd7474
happy <happy@Hemavathi-PC>
2026-07-06 14:58:26 +0530

    SpringBoot Initial Commit

---
+++ opspilot-dashboard/src/main/java/com/hema/opspilot/OpspilotDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.hema.opspilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpspilotDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpspilotDashboardApplication.class, args);
+	}
+
+}
---
+++ opspilot-dashboard/src/test/java/com/hema/opspilot/OpspilotDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hema.opspilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpspilotDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
